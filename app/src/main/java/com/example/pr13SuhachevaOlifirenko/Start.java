bcf290ae5247ecc4a10c859e8410f6845ea9cef8
platinum6356 <vkytkot@gmail.com>
2024-06-20 14:53:45 +0700

    готово

---
+++ app/src/main/java/com/example/pr13SuhachevaOlifirenko/Start.java
@@ -0,0 +1,26 @@
+package com.example.pr13SuhachevaOlifirenko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.pr11Barsuk.R;
+
+public class Start extends AppCompatActivity implements View.OnClickListener {
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        button = findViewById(R.id.start);
+        button.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        Intent intent = new Intent(this, Play.class);
+        startActivity(intent);
+    }
+}
